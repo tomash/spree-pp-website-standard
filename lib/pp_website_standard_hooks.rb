cb3691088cb3969926513bbd31f0b1910680ee3b
Tomasz Stachewicz <tomekrs@o2.pl>
2010-11-12 09:41:55 +0100

    cannot hook paypal partial to checkout/paypal_checkout -- needs to be hooked manually by app developer

--- lib/pp_website_standard_hooks.rb
+++ lib/pp_website_standard_hooks.rb
@@ -1,4 +1,4 @@
 class PpWebsiteStandardHooks < Spree::ThemeSupport::HookListener
   # custom hooks go here
-  insert_before :checkout_summary_box, 'checkout/paypal_checkout'
+  # insert_before :checkout_summary_box, 'checkout/paypal_checkout'
 end
