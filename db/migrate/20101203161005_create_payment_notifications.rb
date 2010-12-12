743b48294552d9c6d509eb319af7e4dda5b02926
Tomasz Stachewicz <tomekrs@o2.pl>
2010-12-12 14:00:43 +0100

    time to refactor this thing. first: migrations

---
+++ db/migrate/20101203161005_create_payment_notifications.rb
@@ -0,0 +1,15 @@
+class CreatePaymentNotifications < ActiveRecord::Migration
+  def self.up
+    create_table :payment_notifications do |t|
+      t.text :params
+      t.string :status
+      t.string :transaction_id
+      t.integer :order_id
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :payment_notifications
+  end
+end
