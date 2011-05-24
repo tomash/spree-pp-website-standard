0c3d645a3c79ba64978b43f770578d08757663a5
Tomasz Stachewicz <tomekrs@o2.pl>
2011-05-24 18:23:48 +0200

    sometimes paypal returns with GET success url, intercept this one as well

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   post "paypal/confirm", :to => "paypal#confirm", :method => :post
+  get "paypal/confirm", :to => "paypal#confirm", :method => :get
   resources :payment_notifications, :only => [:create]
 end
