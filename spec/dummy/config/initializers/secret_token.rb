98943b802b502714fbae2e81819bafc89f05e172
Tomasz Stachewicz <tomekrs@o2.pl>
2011-10-31 00:06:11 +0100

    removed dummy app codebase, as spree guide recommends.

--- spec/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '0896681522addd70a06927bc8483f2739097329e1d40ace758ef0e9bed98b27a0582a4b8fa10ec02a79936a3d259d58febdf746d20daba3e7995d484184eed25'
