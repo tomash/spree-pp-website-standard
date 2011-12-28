d956981fbbe48bb7b3855c5044fdc81cec8b837b
Buddhi DeSilva <buddhi.desilva@gmail.com>
2011-12-28 19:10:35 +0800

    Installation Fixes
    * Updated the installation instructions
    * Removed unwanted code from the installation generator

--- README.md
+++ README.md
@@ -36,11 +36,11 @@ You may want to implement your own custom logic by adding 'state_machine' hooks.
 
 Add to your Spree application Gemfile.
 
-    gem "spree_paypal_website_standard", :git => "git://github.com/tomash/spree-pp-website-standard.git"
+    gem "spree_paypal_website_standard", :git => "git://github.com/buddhi-desilva/spree-pp-website-standard.git"
 
-Run the install rake task to copiy migrations (create payment_notifications table)
+Run the install rake task to copy migrations (create payment_notifications table)
 
-    rake pp_website_standard:install
+    rails generate spree_paypal_website_standard:install
 
 Configure, run, test.
 
--- lib/generators/spree_paypal_website_standard/install/install_generator.rb
+++ lib/generators/spree_paypal_website_standard/install/install_generator.rb
@@ -1,16 +1,6 @@
 module SpreePaypalWebsiteStandard
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      def add_javascripts
-        append_file "app/assets/javascripts/store/all.js", "//= require store/spree-paypal-website-standard\n"
-        append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree-paypal-website-standard\n"
-      end
-
-      def add_stylesheets
-        inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_paypal-website_standard\n", :before => /\*\//, :verbose => true
-        inject_into_file "app/assets/stylesheets/admin/all.css", " *= require admin/spree_paypal-website_standard\n", :before => /\*\//, :verbose => true
-      end
-
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_paypal_website_standard'
       end
