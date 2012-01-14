ec23ef82be796af916e4fbc00905c728bb06f4d2
Buddhi DeSilva <buddhi.desilva@gmail.com>
2012-01-15 07:35:40 +0800

    Tweaks
    * Removed the redundant logic that has been moved to base_controller
    * Removed the logging info
    * Slimmed down the code

--- app/controllers/spree/paypal_controller.rb
+++ app/controllers/spree/paypal_controller.rb
@@ -4,28 +4,20 @@ module Spree
     skip_before_filter :persist_gender
 
     def confirm
-			logger.info("========= Paypal#Confirm ======")
       unless current_order
         redirect_to root_path
       else
         order = current_order
-          while order.state != "complete"
-            order.next
-            state_callback(:after)
-          end
 
-	        if (order.payment_state == "paid") or (order.payment_state == "credit_owed")
-	          flash[:notice] = t(:pp_ws_payment_received)
-	          state_callback(:after)
-						session[:order_id] = nil
-						logger.info("========= PayPal: Order #{order.number} (#{order.id}) Already Paid ======")
-						logger.debug(current_order)
-	        else
-          	flash[:notice] = t(:pp_ws_order_processed_successfully)
-          	flash[:commerce_tracking] = "nothing special"
-	          state_callback(:after)
-        	end
-        redirect_to order_path(current_order)
+        while order.state != "complete"
+          order.next
+          state_callback(:after)
+        end
+
+				flash[:notice] = t(:pp_ws_order_processed_successfully)
+				flash[:commerce_tracking] = "nothing special"
+				state_callback(:after)
+				redirect_to order_path(current_order)
       end
     end
 
