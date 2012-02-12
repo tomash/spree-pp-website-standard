1c7a588eb629fdd17e6f09703c4abedd1f66dbf8
Buddhi DeSilva <buddhi.desilva@gmail.com>
2012-02-13 01:22:20 +0800

    Add: previously missed check on order payment status

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -5,11 +5,13 @@ Spree::BaseController.class_eval do
 	# website with standard paypal return link. The the current order
 	# will be checked for received payments and cart will be reset.
 	def check_current_order
-		if current_order && current_order.line_items.present? && current_order.state == "complete"
+		if current_order && current_order.line_items.present? \
+		&& current_order.state == "complete" \
+		&& ((current_order.payment_state == "paid") or (current_order.payment_state == "credit_owed"))
 			flash[:notice] = t(:pp_ws_payment_received)
 			session[:order_id] = nil
 			redirect_to root_path
-	  end
+		end
 	end
 
 end
