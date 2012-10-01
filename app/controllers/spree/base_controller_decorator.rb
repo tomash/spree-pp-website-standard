6bd2bede0f62a66f681321e10accd35018533919
Buddhi DeSilva <buddhi.desilva@gmail.com>
2012-10-01 17:45:22 +0800

    TO DO: New order complete check mechanism
    * If the user visits a different URL after payment other than use the
    confirm payment url, the user won't get notified on payment. Need a
    fix. Currently commented out since old code is obsolete.

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -3,17 +3,7 @@ Spree::BaseController.class_eval do
 
 	# Checks if an order exists and if its paid
 	# will display the payment success message
-	def check_current_order
-	
-		# Temporary measure to fix the authorisation issue
-		# when a user logs out after adding items to cart
-		# and logs in again and when tries to access the cart
-		# it gives an authorisation failure error.
-		if current_order
-			@order = current_order(true)
-			associate_user
-		end
-
+	def check_current_order	
 		# TODO: Introduce a proper way to check the order payment status
 		# Currently the order get removed from the session the moment the
 		# spree receives payment and no way of tracking. Might have to introduce
@@ -21,20 +11,20 @@ Spree::BaseController.class_eval do
 		# method would be to have session id sent along with IPN secret and
 		# mark a flag on payment notifications after displaying payment received
 		# message
-		if current_order \
-		&& current_order.completed? 
-		# && ((current_order.payment_state == "paid") or (current_order.payment_state == "credit_owed"))
-			flash[:notice] = t(:pp_ws_payment_received)
-			@order = current_order
-			session[:order_id] = nil
+		# if current_order \
+		# && current_order.completed? 
+		# # && ((current_order.payment_state == "paid") or (current_order.payment_state == "credit_owed"))
+		# 	flash[:notice] = t(:pp_ws_payment_received)
+		# 	@order = current_order
+		# 	session[:order_id] = nil
 
-			if current_user
-				redirect_to spree.order_path(@order)
-			else
-				redirect_to root_path
-			end
+		# 	if current_user
+		# 		redirect_to spree.order_path(@order)
+		# 	else
+		# 		redirect_to root_path
+		# 	end
 			
-		end
+		# end
 	end
 
 end
