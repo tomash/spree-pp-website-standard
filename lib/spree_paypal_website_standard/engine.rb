ea83be99c98d772cbb5e37a47098bc565044131e
Tomasz Stachewicz <tomekrs@o2.pl>
2012-12-10 18:22:16 +0100

    Spree 1.1 rolls differently when initializing payment methods

--- lib/spree_paypal_website_standard/engine.rb
+++ lib/spree_paypal_website_standard/engine.rb
@@ -1,6 +1,3 @@
-module Spree::PaypalWebsiteStandard
-end
-
 module SpreePaypalWebsiteStandard
   class Engine < Rails::Engine
     engine_name 'spree_paypal_website_standard'
@@ -16,9 +13,14 @@ module SpreePaypalWebsiteStandard
       Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
-      config.after_initialize do |app|
-        app.config.spree.payment_methods << Spree::BillingIntegration::PaypalWebsiteStandard
-      end
+      # we don't roll like this anymore
+      #config.after_initialize do |app|
+      #  app.config.spree.payment_methods << Spree::BillingIntegration::PaypalWebsiteStandard
+      #end
+    end
+
+    initializer "spree.gateway.payment_methods", :after => "spree.register.payment_methods" do |app|
+      app.config.spree.payment_methods << Spree::BillingIntegration::PaypalWebsiteStandard
     end
 
     config.to_prepare &method(:activate).to_proc
