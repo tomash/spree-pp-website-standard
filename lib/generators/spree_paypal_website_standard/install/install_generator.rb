f76d69dc81222c7e85be058bdd8ee4834c89b4dc
Tomasz Stachewicz <tomekrs@o2.pl>
2014-12-08 15:44:46 +0100

    boilerplate to auto-run migrations when generating dummy app

--- lib/generators/spree_paypal_website_standard/install/install_generator.rb
+++ lib/generators/spree_paypal_website_standard/install/install_generator.rb
@@ -1,13 +1,16 @@
 module SpreePaypalWebsiteStandard
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      class_option :auto_run_migrations, :type => :boolean, :default => false
+
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_paypal_website_standard'
       end
 
       def run_migrations
-        res = ask "Would you like to run the migrations now? [Y/n]"
-        if res == "" || res.downcase == "y"
+        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask 'Would you like to run the migrations now? [Y/n]')
+
+        if run_migrations
           run 'bundle exec rake db:migrate'
         else
           puts "Skiping rake db:migrate, don't forget to run it!"
