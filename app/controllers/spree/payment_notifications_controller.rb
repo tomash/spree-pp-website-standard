6730ab9beb02c3dc72daff8f01e91c8af1a559de
Tomasz Stachewicz <tomekrs@o2.pl>
2014-12-19 15:05:35 +0100

    it's 3.0.0 time, and this means moving on with improvements to payment notifications controller as well

--- app/controllers/spree/payment_notifications_controller.rb
+++ app/controllers/spree/payment_notifications_controller.rb
@@ -4,10 +4,11 @@ module Spree
     skip_before_filter :restriction_access
     
     def create
-      if(Spree::PaypalWebsiteStandard::Config.encrypted && (params[:secret] != Spree::PaypalWebsiteStandard::Config.ipn_secret))
-        logger.info "PayPal_Website_Standard: attempt to send an IPN with invalid secret"
-        raise Exception
-      end
+      payment_method = Order.paypal_payment_method
+      #if(payment_method.preferences[:encryption] && (params[:secret] != payment_method.preferences[:ipn_secret]))
+      #  logger.info "PayPal_Website_Standard: attempt to send an IPN with invalid secret"
+      #  raise Exception
+      #end
       
       @order = Spree::Order.find_by_number(params[:invoice])
       Spree::PaymentNotification.create!(:params => params,
@@ -16,7 +17,6 @@ module Spree
         :transaction_id => params[:txn_id])
       
       logger.info "PayPal_Website_Standard: processing payment notification for invoice #{params["invoice"]}, amount is #{params["mc_gross"]} #{params["mc_currency"]}"
-      # this logging stuff won't live here for long...
       
       Order.transaction do
       # main part of hacks
@@ -25,23 +25,21 @@ module Spree
         #create payment for this order
         payment = Spree::Payment.new
         
-        # 1. Assume that if payment notification comes, it's exactly for the amount
-        # sent to paypal (safe assumption -- cart can't be edited while on paypal)
-        # 2. Can't use Order#total, as it's intercepted by spree-multi-currency
-        # which might lead to lots of false "credit owed" payment states
-        # (when they should be "complete")
-        payment.amount = order.read_attribute(:total)
+        # payment.amount = order.read_attribute(:total)
+        payment.amount = params[:mc_gross].to_f
         logger.info "PayPal_Website_Standard: set payment.amount to #{payment.amount} based on order's total #{order.read_attribute(:total)}"
         
         payment.payment_method = Spree::Order.paypal_payment_method
         order.payments << payment
         payment.started_processing
         
-        order.payment.complete
+        payment.complete!
         logger.info("PayPal_Website_Standard: order #{order.number} (#{order.id}) -- completed payment")
-
-        until @order.state == "complete"
-          if @order.next!
+        @order.reload # otherwise it might not see the newly added payment
+        @order.update!
+        if @order.can_go_to_state?("complete")
+          until @order.state == "complete"
+            @order.next!
             @order.update!
             state_callback(:after)
           end
